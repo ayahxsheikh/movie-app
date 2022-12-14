6f92a2d17d052ba13602b96a55032ba268ec11d3
cc-aeya <aaminah.x@icloud.com>
2022-12-14 14:51:44 +0000

    p tag to show before user searches for a movie

---
+++ movie-app/js/app.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+<div class="movie-item">
+<h3>Movie Title</h3>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+<a href="#">View Details</a>
+</div>
+*/
