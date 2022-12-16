b278036df247b361e8875968e0f21dd3f1b47150
cc-aeya <aaminah.x@icloud.com>
2022-12-16 14:18:34 +0000

    if and else statement for no matches found error

--- movie-app/js/app.js
+++ movie-app/js/app.js
@@ -4,18 +4,24 @@ var itemWrapper = document.body.querySelector('main');
 
 function displayMatches(matches) {
     itemWrapper.innerHTML = '';
-    console.log(matches);
-    for (var matchObj of matches) {
-    itemWrapper.insertAdjacentHTML('beforeend', `
-    <div class="movie-item" style="background: 
-    linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url(${matchObj.Poster})">
-        <h3>${matchObj.Title}</h3>
-        <p>Release Year:${matchObj.Year}</p>
-        <a href="https://www.imdb.com/title/${matchObj.imdbID}" target="_blank">View More Details</a>
-    </div>`
-    );
-  }
+    
+    if(!matches){
+        itemWrapper.innerHTML = '<p class="not-searched">No results found.</p>'
+    } else{
+
+        
+        for (var matchObj of matches) {
+            itemWrapper.insertAdjacentHTML('beforeend', `
+            <div class="movie-item" style="background: 
+            linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+            url(${matchObj.Poster})">
+            <h3>${matchObj.Title}</h3>
+            <p>Release Year:${matchObj.Year}</p>
+            <a href="https://www.imdb.com/title/${matchObj.imdbID}" target="_blank">View More Details</a>
+            </div>`
+            );
+        }
+    }
 
 }
 
